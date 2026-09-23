95802531b7319d9235ba0734b09e32f1ffc49f1b
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-09-24 01:08:35 +0300

    feat(markets): allow disabling market selector query

--- src/features/markets/hooks/use-markets-query.ts
+++ src/features/markets/hooks/use-markets-query.ts
@@ -3,8 +3,9 @@ import { useQuery } from '@tanstack/react-query';
 import { queryKeys } from '@/constants/query-keys';
 import { marketsApi } from '@/features/markets/api/markets-api';
 
-export const useMarketOptionsQuery = () =>
+export const useMarketOptionsQuery = (enabled = true) =>
   useQuery({
     queryKey: queryKeys.users.markets(),
     queryFn: marketsApi.getOptions,
+    enabled,
   });
